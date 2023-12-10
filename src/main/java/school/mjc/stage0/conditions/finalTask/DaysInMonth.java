c5b68e3e71359e1d1fbf73c74a61777174dd7448
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-10 22:54:03 +0200

    finished stage0-module4-conditions-taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 0 || month > 12) {
+            System.out.println("invalid date");
+        }
 
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                System.out.println(31);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                System.out.println(30);
+                break;
+        }
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            if(month == 2) {
+                System.out.println(29);
+            }
+        } else {
+            if(month == 2) {
+                System.out.println(28);
+            }
+        }
     }
 }
